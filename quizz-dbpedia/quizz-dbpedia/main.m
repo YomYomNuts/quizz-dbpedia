a3ea9c4a500deb31c801fc1bb414040136e39fa6
noisette.guillaume <noisette.guillaume@gmail.com>
2013-02-16 12:52:41 +0000

    Mise en place du projet

---
+++ quizz-dbpedia/quizz-dbpedia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  quizz-dbpedia
+//
+//  Created by Lion User on 16/02/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
