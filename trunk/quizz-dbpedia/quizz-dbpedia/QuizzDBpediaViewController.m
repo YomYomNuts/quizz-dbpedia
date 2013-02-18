32265d1b33b63723f73fd25034df5435f41336b6
noisette.guillaume@gmail.com <noisette.guillaume@gmail.com@89929d11-337f-e35f-e3f3-ebe276dcd041>
2013-02-18 00:25:42 +0000

    Ajout d'une première version du parseur de requête json pour dbpedia + ajout de 2 fichiers pour éviter de lancer de trop grosses requêtes

---
+++ trunk/quizz-dbpedia/quizz-dbpedia/QuizzDBpediaViewController.m
@@ -0,0 +1,50 @@
+//
+//  QuizzDBpediaViewController.m
+//  quizz-dbpedia
+//
+//  Created by Lion User on 16/02/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "QuizzDBpediaViewController.h"
+#import "DBpediaFetcher.h"
+
+@interface QuizzDBpediaViewController ()
+
+@end
+
+@implementation QuizzDBpediaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    NSString * query = @"select distinct ?film where {?film <http://dbpedia.org/ontology/starring> ?randomActorApp}";
+    NSDictionary * jsonAnswer = [DBpediaFetcher executeQuery:query];
+    
+    NSLog(@"%@", jsonAnswer);
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
